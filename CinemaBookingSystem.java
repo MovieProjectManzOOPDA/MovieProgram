4d22930e9fd7b42ee073552074caf0a612eb7ad2
digiovann9 <38504090+digiovann9@users.noreply.github.com>
2018-04-28 14:45:36 -0400

    Update CinemaBookingSystem.java

--- CinemaBookingSystem.java
+++ CinemaBookingSystem.java
@@ -5,17 +5,20 @@
 */
 public class CinemaBookingSystem 
 {
-    private ArrayList<Theater> theaters;
-	
+	Theater theater1 = new Theater("theater1");
+	Theater theater2 = new Theater("theater2");
 	public CinemaBookingSystem() 
 	{
 		//constructor
 	}
-	/**
-	* Method responisble for displaying Theaters, Shows, and taking user input.
+	/*
+	* Method responsible for displaying Theaters, Shows, and taking user input.
 	*/
 	public void start()
 	{
-		
+	System.out.println("What theatre are you looking for?");
+	//display the theatres 
+	System.out.println("What show are you looking for?");
+	//display shows
 	}
 }
